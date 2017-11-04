c030cd0bcf84060e3e2bb06ac7968e1f0b961cce
andrei <accs.shershen@gmail.com>
2017-11-04 16:23:22 +0300

    LIB
    Create Hash class and methods

---
+++ HashLib.LIB/ShaHash.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace HashLib.LIB
+{
+    public static class ShaHash
+    {
+        public static string GetHash(string plainText)
+        {
+            if(plainText==null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if(plainText=="")
+            {
+                throw new ArgumentException();
+            }
+            
+            else
+            {
+                using (SHA256 sha = SHA256.Create())
+                {
+                    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(plainText);
+
+                    buffer = sha.ComputeHash(buffer);
+
+                    System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+                    foreach(byte b in buffer)
+                    {
+                        sb.Append(b.ToString("x2"));
+                    }
+
+                    return sb.ToString();
+                }
+            }
+        }
+    }
+}
